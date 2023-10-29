3f0efffe827ee2f7a4a13759cf0544a907754c3f
wujunyao.wjy <wujunyao.wjy@alibaba-inc.com>
2023-10-29 10:02:55 +0800

    fix: 删掉多余信息

--- config/env.py
+++ config/env.py
@@ -2,18 +2,6 @@ from Crypto.Cipher import AES
 import base64
 import os
 
-# daily
-# ak = "ACSTQDkNtSMrZtwL"
-# sk = "zXJ7QF79Oz"
-# endpoint = "xrengine-daily.aliyuncs.com"
-
-#pre
-
-# prod
-ak = "LTAI5tBseK5tDfu4FJvGEQRZ"
-sk = "4RIGB8psCJmRr6h1PY4uedikML95UZ"
-endpoint = "xrengine.cn-hangzhou.aliyuncs.com"
-
-access_key_id = base64.b64decode(os.getenv("Var1", "")).decode("utf-8") or ak
-access_key_secret = base64.b64decode(os.getenv("Var2", "")).decode("utf-8") or sk
-api_endpoint = endpoint
+access_key_id = base64.b64decode(os.getenv("Var1", "")).decode("utf-8")
+access_key_secret = base64.b64decode(os.getenv("Var2", "")).decode("utf-8")
+api_endpoint = "xrengine.cn-hangzhou.aliyuncs.com"
